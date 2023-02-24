df21a3d720899e35c2dca04aa944094658427f12
saraaguilarb <122842897+saraaguilarb@users.noreply.github.com>
2023-02-23 19:26:50 -0600

    tarea 6

---
+++ Tarea6/Tarea6/Tarea6/Models/Calculadora.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea6.Models
+{
+    public class Calculadora
+    {
+        public double number1 { get; set; }
+        public double number2 { get; set; }
+
+        public int CalcularSuma()
+        {
+            int suma = (int)(number1 + number2);
+            return suma;
+        }
+
+        public int CalcularResta()
+        {
+            int resta = (int)(number1 - number2);
+            return resta;
+        }
+
+        public int CalcularMultiplicacion()
+        {
+            int multiplicar = (int)(number1 * number2);
+            return multiplicar;
+        }
+
+        public int CalcularDivision()
+        {
+            int dividir = (int)(number1 / number2);
+            return dividir;
+        }
+
+        public override string ToString()
+        {
+            return $"Resultado: {CalcularSuma()}";
+        }
+    }
+}
